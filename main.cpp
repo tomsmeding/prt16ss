3fa235eeed85e0212501e2e2d27f5ff1d0e3ffe8
tomsmeding <tom.smeding@gmail.com>
2016-04-06 07:42:43 +0200

    switch kaas

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ int main(){
 	Spreadsheet sheet(9,23);
 	SheetView view(sheet);
 	CellAddress cursor(0,0);
+	string lastsavefname;
 	while(true){
 		int c=view.getChar();
 		switch(c){
@@ -23,6 +24,35 @@ int main(){
 						view.redrawCell(addr);
 					}*/
 				}
+				break;
+			}
+			case 's':{
+				Maybe<string> mfname=view.askStringOfUser("Save to file name?",lastsavefname);
+				if(mfname.isNothing()){
+					view.displayStatusString("Not saved.");
+					break;
+				}
+				lastsavefname=mfname.fromJust();
+				if(sheet.saveToDisk(lastsavefname)){
+					view.displayStatusString("Saved.");
+				} else {
+					view.displayStatusString("Error while saving!");
+				}
+				break;
+			}
+			case 'l':{
+				Maybe<string> mfname=view.askStringOfUser("Load from file name?",lastsavefname);
+				if(mfname.isNothing()){
+					view.displayStatusString("Not loaded.");
+					break;
+				}
+				lastsavefname=mfname.fromJust();
+				if(sheet.loadFromDisk(lastsavefname)){
+					view.displayStatusString("Loaded.");
+				} else {
+					view.displayStatusString("Error while loading!");
+				}
+				view.redraw();
 				break;
 			}
 			case KEY_LEFT:
